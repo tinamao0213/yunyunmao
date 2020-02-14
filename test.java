7b19ecb216ac95fd7ce0c663da7d0d1f093dbc12
Your Name <you@example.com>
2020-02-14 12:39:25 +0800

    add hello git change idea22

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println ("hello idea22");
+        System.out.println ("hello git");
+
+    }
+}
